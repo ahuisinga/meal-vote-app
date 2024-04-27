3151ba50883db04a37e223d5eb3e3aba31bc6f38
Anna <anna.huisinga@gmail.com>
2024-04-26 23:45:13 -0500

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="navbar bg-base-200">
-      <div className="dropdown">
-        <div tabIndex={0} role="button" className="btn btn-ghost sm:hidden">
+      <details className="dropdown">
+        <summary className="btn btn-ghost sm:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -19,11 +19,8 @@ export default function Navbar() {
               d="M4 6h16M4 12h8m-8 6h16"
             />
           </svg>
-        </div>
-        <ul
-          tabIndex={0}
-          className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
-        >
+        </summary>
+        <ul className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
           <li>
             <a>Profile</a>
           </li>
@@ -34,7 +31,7 @@ export default function Navbar() {
             <Link href={"/group/join"}>Join a Group</Link>
           </li>
         </ul>
-      </div>
+      </details>
       <div className="flex-1">
         <Link href={"/"} className="btn btn-ghost text-xl italic text-accent">
           EAT!
@@ -49,19 +46,15 @@ export default function Navbar() {
             <Link href={"/group/join"}>Join a Group</Link>
           </li>
         </ul>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="avatar btn btn-circle btn-ghost"
-          >
+        <details className="dropdown dropdown-end">
+          <summary className="avatar btn btn-circle btn-ghost">
             <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
                 src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
               />
             </div>
-          </div>
+          </summary>
           <ul
             tabIndex={0}
             className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-200 p-2 shadow"
@@ -76,7 +69,7 @@ export default function Navbar() {
               <a>Logout</a>
             </li>
           </ul>
-        </div>
+        </details>
       </div>
     </div>
   );
